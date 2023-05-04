687feabd905635aef80a51ee4e7bc5199ef6cff9
Matheus Emanuel <matheusemanuel745@gmail.com>
2023-05-04 15:51:08 -0300

    indexed event fields

--- contracts/NounsIVotedBadge.sol
+++ contracts/NounsIVotedBadge.sol
@@ -5,10 +5,12 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import {NounsDAOLogicV2} from "packages/nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOLogicV2.sol";
 
+import {NounsDAOStorageV2} from "packages/nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOInterfaces.sol";
+
 import {IMetadataRenderer} from "./interfaces/IMetadataRenderer.sol";
 
 
-contract NounsIVotedBadge is ERC721 {
+contract NounsIVotedBadge is ERC721, NounsDAOStorageV2 {
 
   uint256 private _tokenId;
 
@@ -16,12 +18,14 @@ contract NounsIVotedBadge is ERC721 {
 
   IMetadataRenderer public renderer;
 
+  address private _metadataContract;
+
   mapping(uint256 tokenId => uint256 proposalId) private _tokenIdToProposalId;
   mapping(address voter => mapping(uint256 proposalId => bool minted)) private _mintedBadge;
 
   event VoterBadgeMinted(
-    uint256 proposalId,
-    uint256 badgeId
+    uint256 indexed proposalId,
+    uint256 indexed badgeId
   );
 
   error CannotBeZeroAddressError();
@@ -30,40 +34,40 @@ contract NounsIVotedBadge is ERC721 {
   error VoterHasMoreThanZeroVotesError();
   error CannotTransferError();
 
-  constructor(IMetadataRenderer _renderer) ERC721("NOUNS I VOTED", "NIV") {
-    if(address(_renderer) == address(0)) revert CannotBeZeroAddressError();
+  constructor(address _renderer) ERC721("NOUNS I VOTED", "NIV") {
+    if(_renderer == address(0)) revert CannotBeZeroAddressError();
 
-    renderer = _renderer;
+    _metadataContract = _renderer;
   }
 
   
   function claimBadge(uint256 proposalId) external returns (uint256) {
-    if(!NounsDAOLogicV2(NOUNS_DAO).getReceipt(proposalId, msg.sender).hasVoted) revert AddressNotVotedInProposalError(); 
-    if(NounsDAOLogicV2(NOUNS_DAO).getReceipt(proposalId, msg.sender).votes > 0) revert VoterHasMoreThanZeroVotesError();
+
+    Receipt memory receipt = NounsDAOLogicV2(NOUNS_DAO).getReceipt(proposalId, msg.sender);
+
+    if(!receipt.hasVoted) revert AddressNotVotedInProposalError();
+    if(receipt.votes > 0) revert VoterHasMoreThanZeroVotesError();
     if(_mintedBadge[msg.sender][proposalId]) revert AlreadyMintedProposalBadgeError();
 
-    unchecked {
-      _mint(msg.sender, ++_tokenId);
-    }
-
     _mintedBadge[msg.sender][proposalId] = true;
-    _tokenIdToProposalId[_tokenId] = proposalId;
+    _tokenIdToProposalId[++_tokenId] = proposalId;
+
+    unchecked {
+      _mint(msg.sender, _tokenId);
+    }
 
     emit VoterBadgeMinted({
       proposalId: proposalId,
       badgeId: _tokenId
     });
+
+    return _tokenId;
   }
 
+  
   function tokenURI(uint256 tokenId) public view override returns (string memory) {
     _requireMinted(tokenId);
 
-    /*
-    this line:
-
-    is soulbond -> line correct
-    not soulbond -> bug line
-    */
     uint8 voterSupport = NounsDAOLogicV2(NOUNS_DAO).getReceipt(_tokenIdToProposalId[tokenId], ownerOf(tokenId)).support;
 
     return renderer.tokenURI(tokenId, _tokenIdToProposalId[tokenId], voterSupport);
