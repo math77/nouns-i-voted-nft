bbaeb640558d05cb0abaf42ecb6d05a823e073f5
Matheus Emanuel <matheusemanuel745@gmail.com>
2023-05-03 23:27:54 -0300

    delete unused code

--- contracts/NounsIVotedBadgeMetadata.sol
+++ contracts/NounsIVotedBadgeMetadata.sol
@@ -33,9 +33,9 @@ contract NounsIVotedBadgeMetadata is IMetadataRenderer {
           Base64.encode(
             bytes(
               abi.encodePacked(
-                '{"name":"' "Badge #",
+                '{"name":"' "Voter Badge #",
                 tokenId.toString(),
-                '", "description":" Get a badge..."',
+                '", "description":"Collect your voter badge in an ecosystem nouns proposal."',
                 ', "image": "' "data:image/svg+xml;base64,",
                 Base64.encode(bytes(getBadge(proposalId, voterSupport))),
                 '"}'
@@ -45,22 +45,6 @@ contract NounsIVotedBadgeMetadata is IMetadataRenderer {
         )
       );
   }
-
-  /*
-  function badge1(uint256 proposalId, uint8 voterSupport) public view returns (string memory) {
-    return string.concat(
-      '<svg width="450" height="450" viewBox="0 0 450 450" fill="none" xmlns="http://www.w3.org/2000/svg"><style>@font-face {font-family: "Londrina Regular";src: url(data:font/truetype;charset=utf-8;base64,',
-      _londrinaFont.font(),
-      ') format("truetype");} .general {font-family: "Londrina Regular"; fill: #000;}.font-size-1 {font-size: 90px;} .font-size-2 {font-size: 40px;} .noggle {fill: #F3322C;}',
-      noggleColor(voterSupport),
-      ';}</style><circle cx="225" cy="225" r="225" fill="#E1D7D5"/><text x="100" y="180" class="general font-size-1">I VOTED</text><text x="150" y="230" class="general font-size-2">PROP ',
-      proposalId.toString(),
-      '</text>',
-      _noggleSVGs.smallNoggle(),
-      '</svg>'     
-    );    
-  }
-  */
 
   function getBadge(uint256 proposalId, uint8 voterSupport) public view returns (string memory badgeSVG) {
     uint256 badgeType = uint256(keccak256(abi.encodePacked(proposalId, address(this))));
@@ -79,7 +63,8 @@ contract NounsIVotedBadgeMetadata is IMetadataRenderer {
     IFileStore fileStore = IFileStore(_londrinaFontAddress);
     
     //goerli testnet
-    fileStore.getFile("Londrina-Solid-Regular.ttf").read()
+    @font-face {font-family: "Londrina Regular";src: url(data:font/truetype;charset=utf-8;base64,
+    fileStore.getFile("Londrina-Solid-Regular.ttf").read() format("truetype");}
     */
 
     return string.concat(
